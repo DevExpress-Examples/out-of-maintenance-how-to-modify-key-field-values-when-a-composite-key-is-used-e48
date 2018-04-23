d828739474fc5afe212d5eac2fc02187e0d6f71c
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WebSite/Default.aspx
@@ -0,0 +1,56 @@
+<%-- BeginRegion --%>
+<%@ Page Language="C#" AutoEventWireup="true"  CodeFile="Default.aspx.cs" Inherits="Grid_Editing_ModifyKeyFieldValueWhenUsingCompositeKey" %>
+<%@ Register Assembly="DevExpress.Web.v15.1" Namespace="DevExpress.Web" TagPrefix="dxe" %>
+<%@ Register Assembly="DevExpress.Web.v15.1" Namespace="DevExpress.Web" TagPrefix="dxwgv" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<%-- EndRegion --%>
+
+<html xmlns="http://www.w3.org/1999/xhtml" >
+<head runat="server">
+    <title>How to modify key field values when a composite key is used</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        
+        <asp:CheckBox ID="CheckBox1" runat="server" Text="Show CompositeKey Column" AutoPostBack="True" Checked="True" OnCheckedChanged="CheckBox1_CheckedChanged" />
+
+        <dxwgv:ASPxGridView ID="ASPxGridView1" runat="server" AutoGenerateColumns="False"
+            DataSourceID="AccessDataSource1" KeyFieldName="CompositeKey" Width="100%" OnCustomUnboundColumnData="ASPxGridView1_CustomUnboundColumnData" OnRowUpdating="ASPxGridView1_RowUpdating">
+            <Columns>
+                <dxwgv:GridViewCommandColumn VisibleIndex="0" ShowEditButton="True"/>
+                <dxwgv:GridViewDataTextColumn FieldName="CompositeKey" UnboundType="String" VisibleIndex="1" ReadOnly="True">
+                    <CellStyle BackColor="LightYellow">
+                    </CellStyle>
+                </dxwgv:GridViewDataTextColumn>
+                <dxwgv:GridViewDataTextColumn Caption="Title Of Courtesy" FieldName="TitleOfCourtesy"
+                    VisibleIndex="2">
+                </dxwgv:GridViewDataTextColumn>
+                <dxwgv:GridViewDataTextColumn Caption="First Name" FieldName="FirstName" VisibleIndex="3">
+                    <CellStyle BackColor="WhiteSmoke">
+                    </CellStyle>
+                </dxwgv:GridViewDataTextColumn>
+                <dxwgv:GridViewDataTextColumn Caption="Last Name" FieldName="LastName" VisibleIndex="4">
+                    <CellStyle BackColor="WhiteSmoke">
+                    </CellStyle>
+                </dxwgv:GridViewDataTextColumn>
+                <dxwgv:GridViewDataDateColumn Caption="Birth Date" FieldName="BirthDate" VisibleIndex="5" ReadOnly="True">
+                    <CellStyle BackColor="WhiteSmoke">
+                    </CellStyle>
+                </dxwgv:GridViewDataDateColumn>
+                <dxwgv:GridViewDataTextColumn Caption="City" FieldName="City" VisibleIndex="6">
+                </dxwgv:GridViewDataTextColumn>
+            </Columns>
+            <SettingsPager PageSize="5">
+            </SettingsPager>
+            <SettingsEditing Mode="Inline" />
+        </dxwgv:ASPxGridView>
+
+        <%-- BeginRegion DataSource --%>
+        <asp:AccessDataSource ID="AccessDataSource1" runat="server" DataFile="~/App_Data/nwind.mdb"
+            SelectCommand="SELECT [TitleOfCourtesy], [FirstName], [LastName], [BirthDate], [City] FROM [Employees]">
+        </asp:AccessDataSource>
+        <%-- EndRegion --%>
+    </form>
+</body>
+</html>
---
+++ VB/WebSite/Default.aspx.vb
@@ -0,0 +1,67 @@
+Imports Microsoft.VisualBasic
+Imports System
+Imports System.Data
+Imports System.Data.OleDb
+Imports System.Configuration
+Imports System.Web
+Imports System.Web.Security
+Imports System.Web.UI
+Imports System.Web.UI.WebControls
+Imports System.Web.UI.WebControls.WebParts
+Imports System.Web.UI.HtmlControls
+
+Partial Public Class Grid_Editing_ModifyKeyFieldValueWhenUsingCompositeKey
+	Inherits System.Web.UI.Page
+	Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
+
+	End Sub
+
+	Protected Sub ASPxGridView1_CustomUnboundColumnData(ByVal sender As Object, ByVal e As DevExpress.Web.ASPxGridViewColumnDataEventArgs)
+		If e.Column.FieldName = "CompositeKey" Then
+			'Compose a primary key value
+			Dim firstName As String = Convert.ToString(e.GetListSourceFieldValue("FirstName"))
+			Dim lastName As String = Convert.ToString(e.GetListSourceFieldValue("LastName"))
+			Dim birthDate As String = Convert.ToString(e.GetListSourceFieldValue("BirthDate"))
+			e.Value = firstName & "-" & lastName & "-" & birthDate
+		End If
+	End Sub
+
+	Protected Sub ASPxGridView1_RowUpdating(ByVal sender As Object, ByVal e As DevExpress.Web.Data.ASPxDataUpdatingEventArgs)
+		'Create a connection to a data source
+		Dim dataFileName As String = Me.MapPath("..\..\..\App_Data\nwind.mdb")
+		Dim connection As OleDbConnection = New OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" & dataFileName)
+
+		'Define an update command text
+		Dim updateCommandText As String = "UPDATE Employees SET TitleOfCourtesy = @TitleOfCourtesy, FirstName = @FirstName, LastName = @LastName, City = @City WHERE FirstName = @oldFirstName AND LastName = @oldLastName AND BirthDate = @oldBirthDate"
+
+		'Create an update command object
+		Dim updateCommand As OleDbCommand = New OleDbCommand(updateCommandText, connection)
+
+		'Create the update command's required parameters with their values
+		updateCommand.Parameters.AddWithValue("@TitleOfCourtesy", Convert.ToString(e.NewValues("TitleOfCourtesy")))
+		updateCommand.Parameters.AddWithValue("@FirstName", Convert.ToString(e.NewValues("FirstName")))
+		updateCommand.Parameters.AddWithValue("@LastName", Convert.ToString(e.NewValues("LastName")))
+		updateCommand.Parameters.AddWithValue("@City", Convert.ToString(e.NewValues("City")))
+		updateCommand.Parameters.AddWithValue("@oldFirstName", Convert.ToString(e.OldValues("FirstName"))) 'Key field
+		updateCommand.Parameters.AddWithValue("@oldLastName", Convert.ToString(e.OldValues("LastName"))) 'Key field
+		updateCommand.Parameters.AddWithValue("@oldBirthDate", Convert.ToDateTime(e.OldValues("BirthDate"))) 'Key field
+
+		Try
+			'Execute the update command
+			updateCommand.Connection.Open()
+			updateCommand.ExecuteNonQuery()
+		Catch
+			updateCommand.Connection.Close()
+			Throw New InvalidOperationException("Data modifications are not allowed in the online demo")
+		Finally
+			'Cancel the ASPxGridView default update processing 
+			e.Cancel = True
+			'Switch the ASPxGridView to browse mode
+			ASPxGridView1.CancelEdit()
+		End Try
+	End Sub
+
+	Protected Sub CheckBox1_CheckedChanged(ByVal sender As Object, ByVal e As EventArgs)
+		ASPxGridView1.Columns("CompositeKey").Visible = (TryCast(sender, CheckBox)).Checked
+	End Sub
+End Class
